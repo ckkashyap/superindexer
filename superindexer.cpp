5306ed9914a546841141740d9c46dd93df6d3123
kashyap <ckkashyap@gmail.com>
2013-11-17 16:24:04 +0530

    some corrections

--- superindexer.cpp
+++ superindexer.cpp
@@ -26,21 +26,30 @@ int char2int(const char ch) {
 }
 
 
-Block readBlock(std::fstream f) {
+Block readBlockFromFile(std::fstream& f) {
   Block b {};
   f.read(reinterpret_cast<char *>(&b.data), sizeof(b.data));
-  for(auto o : b.offsets) {
+  for(auto& o : b.offsets) {
       f.read(reinterpret_cast<char *>(&o), sizeof(o));
   }
   return b;  
 }
+
+Block writeBlockToFile(Block b, std::fstream& f) {
+  f.write(reinterpret_cast<char *>(&b.data), sizeof(b.data));
+  for(auto o : b.offsets) {
+      f.write(reinterpret_cast<char *>(&o), sizeof(o));
+  }
+  return b;  
+}
+
 
 std::ostream& operator<<(std::ostream& s, Block& b) {
   s << "Block dump" << std::endl;
   s << "\tdata = " << b.data << std::endl;
   int counter = 0;
   for (auto o : b.offsets) {
-    s << "\toffset " << counter << " = " << b.data << std::endl;
+    s << "\toffset " << counter << " = " << o << std::endl;
     counter++;
   }
   return s;
@@ -59,15 +68,38 @@ BlockOffset findWord(std::string word, std::fstream f) {
 }
 
 
+std::fstream initializeFile(const std::string& fn) {
+  std::fstream f {};
+  f.open( fn ,std::ios_base::in | std::ios_base::out | std::ios_base::binary );
+  if (!f.is_open()) {
+    std::cout << "Creating the index file" << std::endl;
+    f.open( fn , std::ios_base::out | std::ios_base::binary );
+    f.close();
+    f.open( fn ,std::ios_base::in | std::ios_base::out | std::ios_base::binary );
+  }
+  return f;
+}
 
 
 
-int main(int argc, char *argv[]) {
-  
+int main(int argc, char *argv[]) {  
   Block b{};
-  std::cout << b << std::endl;
-  std::cout << b.offsets.size() << std::endl;
 
+  b.data=123;
+  int i = 0;
+  for(auto& o : b.offsets) {
+    o = i++;
+  }
+  std::cout << b << std::endl;
+
+  std::string fn {"index.dat"};
+  std::fstream f = initializeFile(fn);
+  writeBlockToFile(b, f);
+  f.close();
+  f = initializeFile(fn);
+  b = readBlockFromFile(f);
+  std::cout << b << std::endl;
+  
   return 0;
 }
 
