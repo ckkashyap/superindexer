ce3165d22a61c7df54186e3c0263bf3ad3c7291b
root <root@no1slu399.adobenet.global.adobe.com>
2013-11-23 11:08:03 +0530

    search works fine

--- search.pl
+++ search.pl
@@ -4,7 +4,7 @@ use strict;
 use warnings;
 
 
-my($numberOfProcesses)=@ARGV;
+
 
 
 sub breakWord {
@@ -16,27 +16,28 @@ sub breakWord {
     return @list;
 }
 
-
-my ($word)=@ARGV[0];
+my($numberOfProcesses, $word)=@ARGV;
 
 my@list=breakWord($word);
 
+for my $i (1..$numberOfProcesses) {
+    my$counter=0;
+    my$indexFile="rootIndex.dat";
+    my$output;
+    for my $part (@list) {
 
-my$counter=0;
-my$indexFile="rootIndex.dat";
-my$output;
-for my $part (@list) {
-
-    next if $part =~ m/^\s*$/;
-    if($counter>0) {
-	$indexFile=sprintf "$indexFile%09d.dat", $output;
+	next if $part =~ m/^\s*$/;
+	if($counter>0) {
+	    $indexFile=sprintf "$i/$indexFile%09d.dat", $output;
+	}
+	$counter++;
+	$output=`./superindexer search $i/$indexFile $part`;
+	chomp$output;
     }
-    $counter++;
-    $output=`../superindexer add $indexFile $part`;
-    chomp$output;
+    next if $output eq "0";
+
+    my$fileName=sprintf "$indexFile%09d.details", $output;
+
+    my@result=`tail -n +2 $i/$fileName`;
+    print @result;
 }
-
-my$fileName=sprintf "$indexFile%09d.details", $output;
-
-my@result=`cat $fileName`;
-print @result;
